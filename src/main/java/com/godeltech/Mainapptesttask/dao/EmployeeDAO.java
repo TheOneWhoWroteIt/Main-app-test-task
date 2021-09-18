33bf06c2d2d7052551d761433d7f1f1ec69ade35
Siarhei Stasiulevich <deform@tut.by>
2021-09-18 17:14:24 +0300

    Create EmployeeDAO and EmployeeDAOImpl
    Interface "EmployeeDAO" contains methods for working with data in the database.
    Class "EmployeeDAOImpl" is an implementation of the interface "EmployeeDAO".

---
+++ src/main/java/com/godeltech/Mainapptesttask/dao/EmployeeDAO.java
@@ -0,0 +1,8 @@
+package com.godeltech.Mainapptesttask.dao;
+
+import com.godeltech.Mainapptesttask.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+}
---
+++ src/main/java/com/godeltech/Mainapptesttask/dao/EmployeeDAOImpl.java
@@ -0,0 +1,10 @@
+package com.godeltech.Mainapptesttask.dao;
+
+import com.godeltech.Mainapptesttask.entity.Employee;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class EmployeeDAOImpl implements EmployeeDAO {
+}
