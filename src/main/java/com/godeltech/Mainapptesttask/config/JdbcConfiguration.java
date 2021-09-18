c7e00ec2fafac4c9265262f63fc3491a8915902c
Siarhei Stasiulevich <deform@tut.by>
2021-09-18 15:32:01 +0300

    Create configuration for database
    Class "JdbcConfiguration" contains settings for working with the database.
    File "postgres.properties" contains settings for connecting to the database.

--- src/main/java/com/godeltech/Mainapptesttask/config/JdbcConfiguration.java
+++ src/main/java/com/godeltech/Mainapptesttask/config/JdbcConfiguration.java
@@ -1,4 +1,38 @@
 package com.godeltech.Mainapptesttask.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("com.godeltech.Mainapptesttask")
+@PropertySource("classpath:postgres.properties")
 public class JdbcConfiguration {
+
+    @Autowired
+    Environment environment;
+
+    @Bean
+    public DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setUrl(environment.getProperty("url"));
+        dataSource.setUsername(environment.getProperty("user"));
+        dataSource.setPassword(environment.getProperty("password"));
+        dataSource.setDriverClassName(environment.getProperty("driver"));
+
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(){
+        return new JdbcTemplate(getDataSource());
+    }
 }
