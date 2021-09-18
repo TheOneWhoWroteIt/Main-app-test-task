f3e9698ec1c1304e44cc4a9fe6e86173302154bc
Siarhei Stasiulevich <deform@tut.by>
2021-09-18 17:48:19 +0300

    Create class EmployeeMapper

---
+++ src/main/java/com/godeltech/Mainapptesttask/entity/EmployeeMapper.java
@@ -0,0 +1,30 @@
+package com.godeltech.Mainapptesttask.entity;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class EmployeeMapper implements RowMapper<Employee> {
+
+    @Override
+    public Employee mapRow(ResultSet resultSet, int i) throws SQLException {
+
+        Employee employee = new Employee();
+        employee.setEmployeeId(resultSet.getLong("employee_id"));
+        employee.setFirstName(resultSet.getString("first_name"));
+        employee.setLastName(resultSet.getString("last_name"));
+        employee.setDepartmentId(resultSet.getLong("department_id"));
+        employee.setJobTitle(resultSet.getString("job_title"));
+
+        Gender gender = Gender.valueOf(resultSet.getString("gender"));
+        LocalDate date = LocalDate.parse(resultSet.getString("day_of_birth"), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
+        employee.setGender(gender);
+        employee.setDateOfBirth(date);
+
+        return employee;
+    }
+}
