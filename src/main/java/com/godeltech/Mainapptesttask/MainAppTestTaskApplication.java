a039966a2ca560cd14c0fa80de004fd2802633d7
Siarhei Stasiulevich <deform@tut.by>
2021-09-18 13:20:54 +0300

    Create project

---
+++ src/main/java/com/godeltech/Mainapptesttask/MainAppTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.godeltech.Mainapptesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainAppTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainAppTestTaskApplication.class, args);
+	}
+
+}
