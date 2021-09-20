e28a913d520a0fbc082a30361f68286d67c2e5c5
Siarhei Stasiulevich <deform@tut.by>
2021-09-20 11:42:16 +0300

    Create class EmployeeServiceTest for test

---
+++ src/test/java/com/godeltech/Mainapptesttask/service/EmployeeServiceTest.java
@@ -0,0 +1,28 @@
+package com.godeltech.Mainapptesttask.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeServiceTest {
+
+    @Test
+    void findAllEmployee() {
+    }
+
+    @Test
+    void findEmployeeById() {
+    }
+
+    @Test
+    void addEmployee() {
+    }
+
+    @Test
+    void updateEmployee() {
+    }
+
+    @Test
+    void deleteEmployee() {
+    }
+}
